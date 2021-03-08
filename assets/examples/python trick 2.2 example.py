361f331020bd45d7f274ea0f7584f022f2d4ab28
hyeonchan <hyeonchan523@gmail.com>
2021-03-08 13:39:21 +0900

    .

--- assets/examples/python trick 2.2 example.py
+++ assets/examples/python trick 2.2 example.py
@@ -1,10 +1,14 @@
-names = ['Alice', 'Bob', 'Dilbert']
+names = ['Alice', 'Bob', 'Dilbert','Jane']
 
 names = ['Alice', 
          'Bob', 
-         'Dilbert']
+         'Dilbert'
+         'Jane'
+         ]
 
 names = ['Alice', 
          'Bob', 
-         'Dilbert',]
+         'Dilbert',
+         'Jane'
+         ]
 
